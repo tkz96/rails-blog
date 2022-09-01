56371c44f935a98b814ec0c51156969bf2885f58
Talha <tkz9696@gmail.com>
2022-09-02 04:20:42 +0500

    Make changes

--- db/migrate/20220901151743_create_comment.rb
+++ db/migrate/20220901151743_create_comment.rb
@@ -2,7 +2,7 @@ class CreateComment < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
       t.text :text
-      t.references :users, null: false, foreign_key: true
+      t.references :author, foreign_key: { to_table: 'users' }
       t.references :posts, null: false, foreign_key: true
 
       t.timestamps
