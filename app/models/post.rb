3688fa15027ddd73016ac808fca0efbc3d0f9d43
Talha <tkz9696@gmail.com>
2022-09-02 18:03:18 +0500

    Add empty custom methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
   has_many :likes
+
+  def update_posts_counter
+
+  end
+
+  def most_recent_comments
+
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_many :posts
   has_many :likes
   has_many :comments
+
+  def most_recent_posts
+
+  end
 end
