30c51278e76bf60e4fcd28dc3d03ce208af7da89
Talha <tkz9696@gmail.com>
2022-09-10 20:19:11 +0500

    Add like specs

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:all) do
+    @first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                              bio: 'Teacher from Mexico.', posts_counter: 1)
+    @post = Post.new(title: 'Hello', text: 'This is my first post', likes_counter: 0, comments_counter: 0,
+                     user: @first_user)
+    @like = Like.new(user: @first_user, post: @post)
+  end
+  it 'creates a valid like' do
+    expect(@like).to be_valid
+  end
 end
