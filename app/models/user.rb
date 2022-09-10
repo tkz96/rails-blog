47d0d931c9876fe72da3547ca81b9a08281bc16f
Talha <tkz9696@gmail.com>
2022-09-10 13:29:14 +0500

    Add user and post model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  validates :name, presence: true
+  validates :posts_counter, comparison: { greater_than_or_equal_to: 0 }
   def three_most_recent_posts
     # A method that returns the 3 most recent posts for a given user.
     posts.find_by(user_id).limit(3)
