b2f69efd6553c6853fa07a74efa04c0ce91081c5
Talha <tkz9696@gmail.com>
2022-09-10 19:15:27 +0500

    Fix linter errors

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-
   before(:all) do
     @first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
                               bio: 'Teacher from Mexico.')
@@ -12,5 +11,4 @@ RSpec.describe Post, type: :model do
   it 'create a post with valid title and length' do
     expect(@post).to be_valid
   end
-
 end
