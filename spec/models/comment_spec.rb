89c823a2dfb41b884a8a357d81f6354632981eab
Talha <tkz9696@gmail.com>
2022-09-10 20:19:51 +0500

    Fix linter errors

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
   before(:all) do
-    @first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', 
+    @first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
                               bio: 'Teacher from Mexico.', posts_counter: 1)
     @post = Post.new(title: 'Hello', text: 'This is my first post', likes_counter: 0, comments_counter: 0,
                      user: @first_user)
